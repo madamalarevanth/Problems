10fba85574ccccdc967be2e79556a78c1a5becdd
REVANTH MADAMALA <revanthrex1001@gmail.com>
2020-06-24 13:37:19 +0530

    Create remove duplicates from unsorted linkedlist2.java

---
+++ remove duplicates from unsorted linkedlist2.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head== null)
+            return null;
+        
+        ListNode current=head.next;
+        ListNode prev= head;
+        
+        
+        while(current!= null)
+        {
+            ListNode runner = head;
+            
+            while(runner!=current)
+            {
+                if(runner.val == current.val)
+                {
+                    ListNode temp = current.next;
+                    prev.next= temp;
+                    current = temp;
+                    break;
+                }
+                runner= runner.next;
+            }
+            if(current==runner)
+            {
+                prev= current;
+                current= current.next;
+            }
+        }
+        
+        return head;
+    }
+}
