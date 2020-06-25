0ba9ce980bd8ca2870fb54876ca11e406f026e30
REVANTH MADAMALA <revanthrex1001@gmail.com>
2020-06-25 11:39:33 +0530

    Create stacksort.java

---
+++ stacksort.java
@@ -0,0 +1,14 @@
+public static Stack<Integer> sort(Stack<Integer> s)
+{
+  Stack<Integer> r = new Stack<Integer>();
+  while(!s.isEmpty())
+  {
+    int tmp = s.pop();
+    while(!r.isEmpty() && r.peek()>tmp)
+    {
+      s.push(r.pop());
+    }
+    r.push(tmp);
+  }
+  return r;
+}
